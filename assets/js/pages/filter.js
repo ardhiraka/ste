f8ecb0264c2cfb4897b2bc8e64bee3d61a453fb8
arispati <masarispati@gmail.com>
2018-09-03 18:54:02 +0700

    calc on edit sms

--- assets/js/pages/filter.js
+++ assets/js/pages/filter.js
@@ -67,6 +67,20 @@ jQuery(function($) {
         $('#smssalah').val(SMS.filtered.inCorrect.join("\n"));
         $('#smsbenar').val(SMS.filtered.correct.join("\n"));
 
+        $.post(ajaxTo('getDataByNumber'), {number: container.data('number')}, response => {
+            if (response) {
+                $('#smsedit').attr('data-user', JSON.stringify(response));
+                
+                calcMessages(SMS, response);
+            } else {
+                alert('Nomor belum terdaftar di database!');
+                
+                $('#hasildapat').val('Nomor belum terdaftar di database!');
+            }
+        }, 'json');
+    });
+
+    let calcMessages = (SMS, response) => {
         let hasil       = [];
         let result      = SMS.matchResult;
         let totalWin    = 0;
@@ -74,62 +88,61 @@ jQuery(function($) {
         let totalPWin   = 0;
         let totalPLose  = 0;
 
-        $.post(ajaxTo('getDataByNumber'), {number: container.data('number')}, response => {
-            for (var i in result) {
-                let theCode = SMS.searchCode(i, true);
-                let thePrice = SMS.searchPrice(i);
+        for (var i in result) {
+            let theCode = SMS.searchCode(i, true);
+            let thePrice = SMS.searchPrice(i);
 
-                if (theCode.code == 'N/A') {
-                    let iWin        = 0;
-                    let iLose       = 0;
-                    let hasilWin    = 0;
-                    let hasilLose   = 0;
-                    let hasilTotal  = 0;
+            if (theCode.code == 'N/A') {
+                let iWin        = 0;
+                let iLose       = 0;
+                let hasilWin    = 0;
+                let hasilLose   = 0;
+                let hasilTotal  = 0;
 
-                    for (let code in result[i]) {
-                        let icode   = result[i][code];
-                        totalWin    += icode.win.length;
-                        totalLose   += icode.lose.length;
-                        hasilWin    += icode.win.length * thePrice * response[code + '_win'];
-                        hasilLose   += (icode.lose.length * thePrice) - ((icode.lose.length * thePrice) * (response[code + '_disc'] / 100));
-                        let cWin    = icode.win.length * thePrice * response[code + '_win'];
-                        let cLose   = (icode.lose.length * thePrice) - ((icode.lose.length * thePrice) * (response[code + '_disc'] / 100));
+                for (let code in result[i]) {
+                    let icode   = result[i][code];
+                    totalWin    += icode.win.length;
+                    totalLose   += icode.lose.length;
+                    hasilWin    += icode.win.length * thePrice * response[code + '_win'];
+                    hasilLose   += (icode.lose.length * thePrice) - ((icode.lose.length * thePrice) * (response[code + '_disc'] / 100));
+                    let cWin    = icode.win.length * thePrice * response[code + '_win'];
+                    let cLose   = (icode.lose.length * thePrice) - ((icode.lose.length * thePrice) * (response[code + '_disc'] / 100));
 
-                        hasil.push(code + ": " + icode.win.length + "/" + icode.lose.length + " | " + formatNumber(cWin) + "/" + formatNumber(cLose));
-                    }
-
-                    totalPWin   = hasilWin;
-                    totalPLose  = hasilLose;
-                    hasilTotal  = hasilWin - hasilLose;
-                } else {
-                    totalWin    += result[i].win.length;
-                    totalLose   += result[i].lose.length;
-                    let theCodeRumusWin     = SMS.code.head.includes(theCode.head) ? 'Jitu' : theCode.code == 'CM' ? 'CM1' : theCode.code;
-                    let theCodeRumusLose    = SMS.code.head.includes(theCode.head) ? 'Jitu' : theCode.code;
-                    let unique      = ['J', 'P', 'T', 'S'];
-                    let rumusWin    = 0;
-                    let rumusLose   = 0;
-
-                    if (unique.includes(theCode.code) || theCode.code == 'M') {
-                        rumusWin    = result[i].win.length > 0 ? result[i].win.length * thePrice * response[theCodeRumusWin + '_win'] : 0;
-                        rumusLose   = result[i].win.length > 0 ? 0 : (result[i].lose.length * thePrice) + ((result[i].lose.length * thePrice) * (response[theCodeRumusLose + '_disc'] / 100));
-                    } else {
-                        rumusWin    = result[i].win.length * thePrice * response[theCodeRumusWin + '_win'];
-                        rumusLose   = (result[i].lose.length * thePrice) - ((result[i].lose.length * thePrice) * (response[theCodeRumusLose + '_disc'] / 100));
-                    }
-
-                    let rumus   = rumusWin - rumusLose;
-                    totalPWin   += rumusWin;
-                    totalPLose  += rumusLose;
-
-                    hasil.push(theCode.full + ": " + result[i].win.length + "/" + result[i].lose.length + " | " + formatNumber(rumusWin) + "/" + formatNumber(rumusLose));
+                    hasil.push(code + ": " + icode.win.length + "/" + icode.lose.length + " | " + formatNumber(cWin) + "/" + formatNumber(cLose));
                 }
-            }
-            hasil.push("Total: " + totalWin + "/" + totalLose + " | " + formatNumber(totalPWin) + "/" + formatNumber(totalPLose) + " | " + formatNumber(totalPWin - totalPLose));
 
-            $('#hasildapat').val(hasil.join("\n"));
-        }, 'json');
-    });
+                totalPWin   = hasilWin;
+                totalPLose  = hasilLose;
+                hasilTotal  = hasilWin - hasilLose;
+            } else {
+                totalWin    += result[i].win.length;
+                totalLose   += result[i].lose.length;
+                let theCodeRumusWin     = SMS.code.head.includes(theCode.head) ? 'Jitu' : theCode.code == 'CM' ? 'CM1' : theCode.code;
+                let theCodeRumusLose    = SMS.code.head.includes(theCode.head) ? 'Jitu' : theCode.code;
+                let unique      = ['J', 'P', 'T', 'S'];
+                let rumusWin    = 0;
+                let rumusLose   = 0;
+
+                if (unique.includes(theCode.code) || theCode.code == 'M') {
+                    rumusWin    = result[i].win.length > 0 ? result[i].win.length * thePrice * response[theCodeRumusWin + '_win'] : 0;
+                    rumusLose   = result[i].win.length > 0 ? 0 : (result[i].lose.length * thePrice) + ((result[i].lose.length * thePrice) * (response[theCodeRumusLose + '_disc'] / 100));
+                } else {
+                    rumusWin    = result[i].win.length * thePrice * response[theCodeRumusWin + '_win'];
+                    rumusLose   = (result[i].lose.length * thePrice) - ((result[i].lose.length * thePrice) * (response[theCodeRumusLose + '_disc'] / 100));
+                }
+
+                let rumus   = rumusWin - rumusLose;
+                totalPWin   += rumusWin;
+                totalPLose  += rumusLose;
+
+                hasil.push(theCode.full + ": " + result[i].win.length + "/" + result[i].lose.length + " | " + formatNumber(rumusWin) + "/" + formatNumber(rumusLose));
+            }
+        }
+
+        hasil.push("Total: " + totalWin + "/" + totalLose + " | " + formatNumber(totalPWin) + "/" + formatNumber(totalPLose) + " | " + formatNumber(totalPWin - totalPLose));
+
+        $('#hasildapat').val(hasil.join("\n"));
+    }
 
     $(document).on('click', '[message-delete]', function() {
         let container   = $(this).closest('tr');
@@ -153,16 +166,20 @@ jQuery(function($) {
 
     let wait = null;
     $('#smsedit').keyup(function() {
-        let message = $(this).val();
+        let message     = $(this).val();
+        let finalNum    = $('#angkaout').val();
+        let dataUser    = $(this).data('user');
 
         if (wait != null) clearTimeout(wait);
 
         wait = setTimeout(function() {
-            SMS.setData(message).filter();
+            SMS.setData(message).filter().parse().setNumber(finalNum).match();
 
             $('#smsasli').val(SMS.data);
             $('#smssalah').val(SMS.filtered.inCorrect.join("\n"));
             $('#smsbenar').val(SMS.filtered.correct.join("\n"));
+
+            calcMessages(SMS, dataUser);
         }, 500);
     });
 
