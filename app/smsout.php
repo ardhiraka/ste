d8ae6b4930a47a98c54e45f97bf20d4160a5c8d5
arispati <masarispati@gmail.com>
2018-09-22 13:17:40 +0700

    smsout

--- app/smsout.php
+++ app/smsout.php
@@ -2,6 +2,7 @@
 include('header.php');
 
 $lists = $db->fetch_all("SELECT so.id as so_id, so.inbox_id, so.member_id, SUM(so.win) AS win, SUM(so.lose) AS lose, SUM(so.total) AS total, SUM(so.total_makan) AS total_makan, SUM(so.total_dealer) AS total_dealer, m.nohp, m.kodeid FROM sms_out AS so LEFT JOIN member AS m ON m.id = so.member_id GROUP BY so.member_id ORDER BY so.id ASC");
+$listPotongan = $db->fetch_all("SELECT so.id as so_id, so.inbox_id, so.member_id, SUM(so.win) AS win, SUM(so.lose) AS lose, SUM(so.total) AS total, SUM(so.total_makan) AS total_makan, SUM(so.total_dealer) AS total_dealer, m.nohp, m.kodeid FROM sms_out AS so LEFT JOIN member AS m ON m.id = so.member_id");
 
 ?>
 
@@ -55,18 +56,14 @@ $lists = $db->fetch_all("SELECT so.id as so_id, so.inbox_id, so.member_id, SUM(s
 				<table class="table table-bordered">
 					<thead>
 						<tr>
-							<th>Nomor HP</th>
-							<th>Kode</th>
-							<th>Win</th>
-							<th>Lose</th>
+							<th>Total Win</th>
+							<th>Total Lose</th>
 							<th>Total Transaksi</th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php foreach($lists as $sms) : ?>
+						<?php foreach($listPotongan as $sms) : ?>
 							<tr>
-								<td><?= $sms['nohp'] ?></td>
-								<td><?= $sms['kodeid'] ?></td>
 								<td><?= $sms['win'] ?></td>
 								<td><?= $sms['lose'] ?></td>
 								<td>
@@ -88,18 +85,14 @@ $lists = $db->fetch_all("SELECT so.id as so_id, so.inbox_id, so.member_id, SUM(s
 				<table class="table table-bordered">
 					<thead>
 						<tr>
-							<th>Nomor HP</th>
-							<th>Kode</th>
-							<th>Win</th>
-							<th>Lose</th>
+							<th>Total Win</th>
+							<th>Total Lose</th>
 							<th>Total Transaksi</th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php foreach($lists as $sms) : ?>
+						<?php foreach($listPotongan as $sms) : ?>
 							<tr>
-								<td><?= $sms['nohp'] ?></td>
-								<td><?= $sms['kodeid'] ?></td>
 								<td><?= $sms['win'] ?></td>
 								<td><?= $sms['lose'] ?></td>
 								<td>
